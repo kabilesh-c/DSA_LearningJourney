acbb286973d709142d24d5ab7982161b9ed48149
Kabilesh C <kc7002@srmist.edu.in>
2026-06-04 22:59:17 +0530

    completed 274. H-Index leetcode question

---
+++ 274. H-Index.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public int hIndex(int[] citations) {
+
+        Arrays.sort(citations);
+
+        int n = citations.length;
+
+        for(int i = 0; i < n; i++) {
+
+            if(citations[i] >= n - i) {
+                return n - i;
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int[] citations = {3, 0, 6, 1, 5};
+
+        System.out.println(obj.hIndex(citations));
+    }
+}
