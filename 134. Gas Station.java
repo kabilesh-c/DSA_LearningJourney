7ce7a78a7b385e3ca835f1b55ef2f9da9fe20378
Kabilesh C <kc7002@srmist.edu.in>
2026-06-05 23:51:50 +0530

    completed 134. Gas Station

--- 134. Gas Station.java
+++ 134. Gas Station.java
@@ -1,3 +1,48 @@
-public class 134. Gas Station {
-    
+class Solution {
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+        int totalGas = 0;
+        int totalCost = 0;
+
+        for(int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+
+        if(totalGas < totalCost) {
+            return -1;
+        }
+
+        int start = 0;
+        int tank = 0;
+
+        for(int i = 0; i < gas.length; i++) {
+
+            tank += gas[i] - cost[i];
+
+            if(tank < 0) {
+
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        return start;
+    }
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int[] gas1 = {1, 2, 3, 4, 5};
+        int[] cost1 = {3, 4, 5, 1, 2};
+
+        System.out.println(obj.canCompleteCircuit(gas1, cost1));
+
+        int[] gas2 = {2, 3, 4};
+        int[] cost2 = {3, 4, 3};
+
+        System.out.println(obj.canCompleteCircuit(gas2, cost2));
+    }
 }
