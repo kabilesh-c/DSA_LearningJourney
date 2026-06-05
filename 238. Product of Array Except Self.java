c0696eca4de8154538612ee27fd071addf2291b9
Kabilesh C <kc7002@srmist.edu.in>
2026-06-05 23:46:08 +0530

    completed 380. Insert Delete GetRandom O(1)

---
+++ 238. Product of Array Except Self.java
@@ -0,0 +1,3 @@
+public class 238. Product of Array Except Self {
+    
+}
---
+++ 380. Insert Delete GetRandom O(1).java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+class Solution {
+
+    private ArrayList<Integer> list;
+    private HashMap<Integer, Integer> map;
+    private Random random;
+
+    public Solution() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+
+        if(map.containsKey(val)) {
+            return false;
+        }
+
+        list.add(val);
+        map.put(val, list.size() - 1);
+
+        return true;
+    }
+
+    public boolean remove(int val) {
+
+        if(!map.containsKey(val)) {
+            return false;
+        }
+
+        int index = map.get(val);
+
+        int lastElement = list.get(list.size() - 1);
+
+        list.set(index, lastElement);
+
+        map.put(lastElement, index);
+
+        list.remove(list.size() - 1);
+
+        map.remove(val);
+
+        return true;
+    }
+
+    public int getRandom() {
+
+        int randomIndex = random.nextInt(list.size());
+
+        return list.get(randomIndex);
+    }
+
+    public static void main(String[] args) {
+
+        Solution randomizedSet = new Solution();
+
+        System.out.println(randomizedSet.insert(1));    
+
+        System.out.println(randomizedSet.remove(2));     
+
+        System.out.println(randomizedSet.insert(2));     
+
+        System.out.println(randomizedSet.getRandom());   
+
+        System.out.println(randomizedSet.remove(1));    
+
+        System.out.println(randomizedSet.insert(2));     
+
+        System.out.println(randomizedSet.getRandom());   
+    }
+}
