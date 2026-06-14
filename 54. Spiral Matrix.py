7f78f660289cb410862cba8a9711b6acc3f1598b
Kabilesh C <kc7002@srmist.edu.in>
2026-06-14 12:00:00 +0530

    completed 54. Spiral Matrix

---
+++ 54. Spiral Matrix.py
@@ -0,0 +1,45 @@
+class Solution:
+
+    def spiralOrder(self, matrix):
+
+        result = []
+
+        top = 0
+        bottom = len(matrix) - 1
+
+        left = 0
+        right = len(matrix[0]) - 1
+
+        while left <= right and top <= bottom:
+
+            for col in range(left, right + 1):
+                result.append(matrix[top][col])
+
+            top += 1
+
+            for row in range(top, bottom + 1):
+                result.append(matrix[row][right])
+
+            right -= 1
+
+            if top <= bottom:
+
+                for col in range(right, left - 1, -1):
+                    result.append(matrix[bottom][col])
+
+                bottom -= 1
+
+            if left <= right:
+
+                for row in range(bottom, top - 1, -1):
+                    result.append(matrix[row][left])
+
+                left += 1
+
+        return result
+
+
+obj = Solution()
+
+print(obj.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
+print(obj.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
