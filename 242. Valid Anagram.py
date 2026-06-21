80daffe7b986d698b7885ef83000233b84bfd605
Kabilesh C <kc7002@srmist.edu.in>
2026-06-21 23:34:01 +0530

    242. Valid Anagram

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def isAnagram(self, s, t):
+
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+
+        for ch in t:
+
+            if ch not in count:
+                return False
+
+            count[ch] -= 1
+
+            if count[ch] < 0:
+                return False
+
+        return True
+
+
+obj = Solution()
+
+print(obj.isAnagram("anagram", "nagaram"))
+print(obj.isAnagram("rat", "car"))
