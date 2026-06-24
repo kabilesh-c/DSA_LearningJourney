f7950ac662126a3efd3c6ad76df5475d920bdaf0
Kabilesh C <kc7002@srmist.edu.in>
2026-06-24 21:38:47 +0530

    202. Happy Number

---
+++ 202. Happy Number.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def isHappy(self, n):
+
+        seen = set()
+
+        while n != 1:
+
+            if n in seen:
+                return False
+
+            seen.add(n)
+
+            total = 0
+
+            while n > 0:
+
+                digit = n % 10
+
+                total += digit * digit
+
+                n //= 10
+
+            n = total
+
+        return True
+
+
+obj = Solution()
+
+print(obj.isHappy(19))
+print(obj.isHappy(2))
